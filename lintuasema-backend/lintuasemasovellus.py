330851a2911642f7750967fc7c3f3c581da82e27
Emil Andersson <emil.andersson@helsinki.fi>
2020-09-15 00:18:55 +0300

    Getting persontoken from login redirect request

--- lintuasema-backend/lintuasemasovellus.py
+++ lintuasema-backend/lintuasemasovellus.py
@@ -11,23 +11,24 @@ AUTH_TOKEN = os.getenv('AUTH_TOKEN')
 TARGET = os.getenv('TARGET')
 
 app = init_app()
-port = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 3000))
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='127.0.0.1', port=port)
 
 @app.route('/login', methods=['POST', 'GET'])
 def loginconfirm():
-    #ota personToken talteen tässä ja tee sillä jotain
-    #personToken löytyy url-parametrista nimeltä token
-    return "Hello"
+    personToken = request.form['token']
+    #nextParam = request.form['next'] ei ilmeisesti toimi?
+
+    return "Metodi: " + request.method + ', personToken: ' + personToken 
 
 
 @app.route('/loginRedirect', methods=['POST', 'GET'])
 def login():
-    return redirect('https://fmnh-ws-test.it.helsinki.fi/laji-auth/login?target=%s&redirectMethod=GET&next=http://localhost:3000/' % (TARGET))
-    #return redirect('https://login-dev.laji.fi/login?target=%s&redirectMethod=POST&next=http://127.0.0.1:3000/' % (TARGET))
+    # return redirect('https://fmnh-ws-test.it.helsinki.fi/laji-auth/login?target=%s&redirectMethod=GET&next=http://localhost:3000/' % (TARGET))
+    return redirect('https://login-dev.laji.fi/login?target=%s&redirectMethod=POST&next=/index' % (TARGET))
