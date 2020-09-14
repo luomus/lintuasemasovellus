ad78472b0f427437abf5501b7480821b20736767
Sonja Heikkinen <sonja.t.heikkinen@hotmail.com>
2020-09-14 21:00:24 +0300

    Move redirect to laji_auth_login to route loginRedirect instead of login (fix infinite loop problem in authentication)

--- lintuasema-backend/lintuasemasovellus.py
+++ lintuasema-backend/lintuasemasovellus.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 import requests
+import urllib.parse as urlparse
 from flask import Flask, render_template, request, redirect, session, url_for
 
 
@@ -20,8 +21,13 @@ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=port)
 
 @app.route('/login', methods=['POST', 'GET'])
+def loginconfirm():
+    #ota personToken talteen tässä ja tee sillä jotain
+    #personToken löytyy url-parametrista nimeltä token
+    return "Hello"
+
+
+@app.route('/loginRedirect', methods=['POST', 'GET'])
 def login():
-    return redirect('https://fmnh-ws-test.it.helsinki.fi/laji-auth/login?target=%s&redirectMethod=POST&next=http://localhost:3000/' % (TARGET))
+    return redirect('https://fmnh-ws-test.it.helsinki.fi/laji-auth/login?target=%s&redirectMethod=GET&next=http://localhost:3000/' % (TARGET))
     #return redirect('https://login-dev.laji.fi/login?target=%s&redirectMethod=POST&next=http://127.0.0.1:3000/' % (TARGET))
-
-
