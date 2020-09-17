29e59aa97d1d5904507b4547a107fe9c653e0526
Jyrki <jyrki.kaukoranta@helsinki.fi>
2020-09-17 21:17:19 +0300

    try expectit lisatty

--- lintuasema-backend/app/__init__.py
+++ lintuasema-backend/app/__init__.py
@@ -17,19 +17,27 @@ import app.oracleConfig
 def init_app():
     
     app = Flask(__name__, static_folder='../build', static_url_path='/')
-    return app
 
     dnsStr = cx_Oracle.makedsn('oracle.luomus.fi', 1521, service_name='oracle.luomus.fi')
     dnsStr = dnsStr.replace('SID', 'SERVICE_TYPE')
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = "oracle://"+oracleConfig.username+":"+oracleConfig.password+"@"+dnsStr
-    app.config["SQLALCHEMY_ECHO"] = True
-
+    
+    try:
+        app.config["SQLALCHEMY_DATABASE_URI"] = "oracle://"+oracleConfig.username+":"+oracleConfig.password+"@"+dnsStr
+        app.config["SQLALCHEMY_ECHO"] = True
+        print('Tietokantayhteys luotu.')
+    except Exception as e:
+        print(e)
+    
+    
 
     app.register_blueprint(api_blueprint)
     
     db.init_app(app) #db siirretty omaksi luokaksi, että se näkyy kaikille, jostain syystä init_app() systeemillä tehtäessä se ei näy. kaikkiin models.py tiedostoihin from app.db import db
     with app.app_context(): #appioliota käyttäen luodaan tietokantataulut, tämä googlesta
-        db.create_all()
-  
+       try:
+           db.create_all()
+           print('Taulut luotu')
+       except Exception as e:
+           print(e)
+
     return app
