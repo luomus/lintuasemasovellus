017da54c1d07760cedc31271c83d38e598910a2f
Robert <robert.j.rantanen@helsinki.fi>
2020-09-17 12:55:48 +0300

    add oracleconfig, secrets now stored in env variables

---
+++ lintuasema-backend/app/oracleConfig.py
@@ -0,0 +1,8 @@
+import os
+
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+hostname = os.getenv('HOSTNAME')
+serviceid  = os.getenv('SERVICEID')
+port = 1521
+encoding = 'UTF-8'
