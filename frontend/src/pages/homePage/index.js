b490aa93340c869e0e29e845e29193c9b4986c48
Ville T <v.taski@gmail.com>
2020-09-20 13:22:13 +0300

    update frontpage with a paper component

--- frontend/src/pages/homePage/index.js
+++ frontend/src/pages/homePage/index.js
@@ -1,11 +1,30 @@
 import React from "react";
+import {Paper, Grid} from "@material-ui/core/";
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+  paper: {
+    background: "white",
+    padding: "20px 30px",
+  },
+});
+
+
 
 export const HomePage = () => {
+  const classes = useStyles();
+
   return (
     <div>
-      <p>
-        Text on main page.
-      </p>
+       <Grid container spacing={40}>
+    <Grid item xs={12} sm={6}>
+      <Paper className={classes.paper}>Tämä on täytetekstiä. Suomessa sijaitsee 16 lintuasemaa. 
+      Lintuasemasovelluksen avulla käyttäjä voi kirjata lintuasemilla tehtyjä
+      lintujen havaintotietoja vakioidussa muodossa. Havaintojen tallentaminen järjestelmään vaatii
+      sisäänkirjautumisen.</Paper>
+    </Grid>
+  </Grid>
     </div>
   );
 };
