c2ac33cc1e486c9fb123fd758f9178b18d7cc9d9
Ville T <v.taski@gmail.com>
2020-10-02 22:47:46 +0300

    edited daylist colors and hover effect

--- frontend/src/pages/dayList/index.js
+++ frontend/src/pages/dayList/index.js
@@ -1,12 +1,32 @@
-import { Table, TableBody, TableCell, TableHead, TableRow, Typography } from "@material-ui/core";
+import {
+   Paper, withStyles, makeStyles, Table, TableBody,
+   TableCell, TableHead, TableRow,
+   Typography } from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import ObsStation from "../../globalComponents/ObsStation";
 import { getDays } from "../../services";
 
+const useStyles = makeStyles({
+  paper: {
+    background: "white",
+    padding: "20px 30px",
+  },
+});
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: "grey",
+    color: "white",
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
 
 export const DayList = () => {
   const [list, setList] = useState([]);
-
+  const classes = useStyles();
 
   useEffect(() => {
     getDays()
@@ -20,40 +40,44 @@ export const DayList = () => {
 
   return (
     <div>
-      <Typography variant="h5" component="h2" >
-        Päivät
-      </Typography>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Päivä</TableCell>
-            <TableCell align="right">Havainnoijat</TableCell>
-            <TableCell align="right">Kommentti</TableCell>
-            <TableCell align="right">Havainnointiasema</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {
-            list.map((s, i) =>
-              <TableRow key={i}>
-                <TableCell component="th" scope="row">
-                  {s.day}
-                </TableCell>
-                <TableCell align="right">
-                  {s.observers}
-                </TableCell>
-                <TableCell align="right">
-                  {s.comment}
-                </TableCell>
-                <TableCell align="right">
-                  <ObsStation id={s.observatory} />
-                </TableCell>
+      <Paper className={classes.paper}>
 
-              </TableRow>
-            )
-          }
-        </TableBody>
-      </Table>
+        <Typography variant="h5" component="h2" >
+          Päivät
+      </Typography>
+      <br />
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Päivämäärä</StyledTableCell>
+              <StyledTableCell align="right">Havainnoijat</StyledTableCell>
+              <StyledTableCell align="right">Kommentit</StyledTableCell>
+              <StyledTableCell align="right">Havainnointiasema</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              list.map((s, i) =>
+                <TableRow hover key={i}>
+                  <StyledTableCell component="th" scope="row">
+                    {s.day}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {s.observers}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    {s.comment}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    <ObsStation id={s.observatory} />
+                  </StyledTableCell>
+
+                </TableRow>
+              )
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     </div>
   );
 };
