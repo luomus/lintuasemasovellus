cfff500f586171cd32b18c53e8309effd8a9e0ec
Ville T <v.taski@gmail.com>
2020-10-03 16:01:07 +0300

    implement i18next

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
+import './i18n';
 
 ReactDOM.render(
   <Provider store={store}>
