c2837df3e36f89ea6b7684877e06c391fb48885b
Jyrki <jyrki.kaukoranta@helsinki.fi>
2020-10-09 13:11:16 +0300

    addday validation added

--- lintuasema-backend/app/api/classes/day/services.py
+++ lintuasema-backend/app/api/classes/day/services.py
@@ -3,11 +3,14 @@ from app.api.classes.day.models import Day
 from app.db import db
 
 def addDay(day):
-    if day.observatory_id is not None and day.day is not None and day.observers is not None:
+    d = Day.query.filter_by(day = day.day, observatory_id = day.observatory_id).first()
+    if not d and day.observatory_id is not None and day.day is not None and day.observers is not None:
         db.session().add(day)
         db.session().commit()
+        return True
+    else:
+        return False
 
 def getDays():
     dayObjects = Day.query.all()
     return dayObjects
-
