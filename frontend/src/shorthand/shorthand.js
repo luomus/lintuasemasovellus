df9b78e21de85e139ff1a95ab590828a9dcc0e27
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-10-23 10:17:44 +0300

    Make some algorithm and test it

--- frontend/src/shorthand/shorthand.js
+++ frontend/src/shorthand/shorthand.js
@@ -1,112 +1,343 @@
 
-let periods = [];
+// Buckets:
+let firstSpaceOfLineBreakOrSomeSuchEncountered = false;
+let lisatietobucketIsOpen = false;
+let lajinimi = "";
 
-
-
-let startTime = "";
-let endTime = "";
-let species = "";
-
-
-const reset = () => {
-  startTime = "";
-  endTime = "";
-  species = "";
-
-};
-
-/*let kelloasiat = "";
 let lisatieto = "";
+let yksilomaara = "";
 let ika = "";
+
 let ohituspuoli = "";
 let ilmansuunta = "";
+let sukupuoliRound = 2;
 
-const group = (char) => {
+
+let ageBucket = "unk";
+
+let male = {
+  "subad": "",
+  "ad": "",
+  "juv": "",
+  "unk": ""
+};
+
+let female = {
+  "subad": "",
+  "ad": "",
+  "juv": "",
+  "unk": ""
+};
+
+let unknown = {
+  "subad": "",
+  "ad": "",
+  "juv": "",
+  "unk": ""
+};
+
+let osahavainnot = [];
+
+let taysiHavainto = {};
+
+const openLisatietoBucket = () => {
+  lisatietobucketIsOpen = true;
+};
+
+const closeLisatietoBucket = () => {
+  lisatietobucketIsOpen = false;
+};
+
+const softReset = () => {
+  yksilomaara = "";
+  ika = "";
+  ageBucket = "unk";
+  sukupuoliRound = 2;
+  ilmansuunta = "";
+  ohituspuoli = "";
+  lisatieto = "";
+};
+
+const constructOsahavainto = () => {
+  switch(sukupuoliRound) {
+    case 0:
+      male[String(ageBucket)] = yksilomaara;
+      break;
+    case 1:
+      female[String(ageBucket)] = yksilomaara;
+      break;
+    case 2:
+      unknown[String(ageBucket)] = yksilomaara;
+      break;
+    default:
+      throw new Error("illegal sukupuoliround");
+  }
+  const osahavainto = {
+    adultUnknownCount: unknown["ad"],
+    adultFemaleCount: female["ad"],
+    adultMaleCount: male["ad"],
+    juvenileUnknownCount: unknown["juv"],
+    juvenileFemaleCount: female["juv"],
+    juvenileMaleCount: male["juv"],
+    subadultUnknownCount: unknown["subad"],
+    subadultFemaleCount: female["subad"],
+    subadultMaleCount: male["subad"],
+    unknownUnknownCount: unknown["unk"],
+    unknownMaleCount: male["unk"],
+    unknownFemaleCount: female["unk"],
+    direction: ilmansuunta,
+    bypassSide: ohituspuoli,
+    notes: lisatieto
+  };
+  osahavainnot.push(osahavainto);
+  softReset();
+};
+
+const acceptableIka = new Set(["subad", "ad", "juv", "'", "\""]);
+
+
+const ikaConstructed = () => {
+  return acceptableIka.has(ika);
+};
+
+export const constructTaysiHavainto = (startKello, endKello) => {
+  constructOsahavainto();
+  taysiHavainto = {
+    species: lajinimi,
+    startTime: startKello,
+    endTime: endKello,
+    osahavainnot
+  };
+};
+
+const lajinimiNotSet = () => {
+  return !firstSpaceOfLineBreakOrSomeSuchEncountered;
+};
+
+const setAgeBucket = () => {
+  if (ika === "'") {
+    ageBucket = "juv";
+  } else if (ika === "\"") {
+    ageBucket = "ad";
+  } else {
+    ageBucket = ika;
+  }
+  ika = "";
+};
+
+const acceptableIkaChar = new Set(["j","v","b","d","e","u","s","a"]);
+const acceptlableIlmansuuntaChar = new Set(["n","w","e","s"]);
+
+const isNumeric = (string) => {
+  // eslint-disable-next-line eqeqeq
+  if (typeof string != "string") return false;
+  return !isNaN(string) && !isNaN(parseInt(string));
+};
+
+const acceptableAroundIlmansuuntaHeuristic = (index, line) => {
+  if (index === line.length - 1) return true;
+  if (index === 0) return false;
+  if (acceptableIkaChar.has(line[index+1])) {
+    return line[index+1] === "s" || line[index+1] === "e";
+  }
+  let i = index+1;
+  while (line[Number(i)] === " " && i < line.length) {
+    ++i;
+  }
+  if (line[Number(i)] === ",") return true;
+  if (isNumeric(line[Number(i)])) throw new Error("suunnan jälkeen tulee numero");
+  return true;
+};
+
+const fillSukupuoliBucketsSlash = () => {
+  switch (sukupuoliRound) {
+    case 0:
+      break;
+    case 1:
+      female[String(ageBucket)] = yksilomaara;
+      sukupuoliRound = 2;
+      break;
+    case 2:
+      male["ad"] = unknown["ad"];
+      male["juv"] = unknown["juv"];
+      male["subad"] = unknown["subad"];
+      male["unk"] = unknown["unk"];
+      male[String(ageBucket)] = yksilomaara;
+      unknown["ad"] = "";
+      unknown["subad"] = "";
+      unknown["juv"] = "";
+      unknown["unk"] = "";
+      sukupuoliRound = 1;
+      break;
+    default:
+      throw new Error("illegal sukupuoliRound");
+  }
+  yksilomaara = "";
+  ageBucket = "unk";
+};
+
+const fillSukupuoliBucketsNotSlash = () => {
+  switch (sukupuoliRound) {
+    case 0:
+      male[String(ageBucket)] = yksilomaara;
+      break;
+    case 1:
+      female[String(ageBucket)] = yksilomaara;
+      break;
+    case 2:
+      unknown[String(ageBucket)] = yksilomaara;
+      break;
+    default:
+      throw new Error("illegal sukupuoliRound");
+  }
+  yksilomaara = "";
+  ageBucket = "unk";
+};
+
+const isTooManyCommasHeuristic = (index, line) => {
+  let i = index+1;
+  while (line[Number(i)] === " " && i < line.length) {
+    ++i;
+  }
+  return line[Number(i)] === ",";
+};
+
+/**
+ * Organize chars to buckets.
+ * @param {character} char
+ * @param {string} line
+ * @param {integer} index
+ */
+export const giveMeABucket = (char, line, index) => {
+  if (ikaConstructed()) {
+    setAgeBucket();
+    fillSukupuoliBucketsNotSlash();
+  }
   switch(char) {
-    case " ":case "\n":
+    default:
+      if (lajinimiNotSet()) {
+        lajinimi += char;
+      } else if (lisatietobucketIsOpen) {
+        lisatieto += char;
+      } else if (((acceptlableIlmansuuntaChar.has(char) && !acceptableIkaChar.has(char))
+        || (acceptlableIlmansuuntaChar.has(char) && (char === "s" || char === "e")))
+        && acceptableAroundIlmansuuntaHeuristic(index, line)) {
+        ilmansuunta += char;
+      } else if (acceptableIkaChar.has(char)) {
+        ika += char;
+      } else {
+        throw new Error("probably illegal char:", char);
+      }
+      break;
+    case " ":case "\t":case "\n":
+      firstSpaceOfLineBreakOrSomeSuchEncountered = true;
       break;
     case "0":case "1":case "2":
     case "3":case "4":case "5":
     case "6":case "7":case "8":
     case "9":
-      if (kello()) {
-        kelloasiat+=char;
-      } else {
-        yksilomaara+=char;
-      }
+      yksilomaara += char;
       break;
     case ".":
+      // ignore (although time should not be on the line)
       break;
     case "(":
-      lisatieto(true);
-      lisatieto += char;
+      openLisatietoBucket();
       break;
     case ")":
-      lisatieto(false);
-      lisatieto += char;
+      closeLisatietoBucket();
       break;
     case ",":
-      // lisää havainto
+      //osahavainto
+      if (isTooManyCommasHeuristic(index, line))
+        throw new Error("ylimääräisiä pilkkuja");
+      constructOsahavainto();
       break;
-    case "‘":case "“":case "’":
-      break;
-    case "j":case "u":
-    case "v":case "s":
-    case "b":case "a":
-    case "d":case "n":
-    case "e":case "w":
-      if(ilmansuunta())
-        ilmansuunta+=char;
-      else ika+=char;
+    case "'":case "\"":
+      ika += char;
       break;
     case "+":case "-":
-      ohituspuoli+=char;
+      if (index < line.length - 1
+          && char === "-" && line[index + 1] === "+") {
+        throw new Error("tuntematon ohituspuoli");
+      }
+      if (index < line.length - 1
+          && line[index + 1] !== ","
+          && line[index + 1] !== "\n"
+          && line[index + 1] !== "+"
+          && line[index + 1] !== "-") {
+        throw new Error("ohituspuolen pitää olla viimeisenä");
+      }
+      ohituspuoli += char;
       break;
     case "/":
+      fillSukupuoliBucketsSlash();
       break;
   }
-};*/
-
-const timeRegex = new RegExp(/^(([01][0-9])|(2[0-3]))(:|\.)[0-5][0-9]$/);
-
-export const parse = (text) => {
-
-  const lines = text.split("\n");
-  for (const line of lines) {
-    if (line.match(timeRegex)) {
-      if (!startTime) {
-        startTime = line;
-      } else {
-        endTime = line;
-        periods.push({
-          startTime,
-          endTime,
-          species,
-        });
-        reset();
-      }
-    } else {
-      let i = 0;
-      for (; i < line.length; i++) {
-        if (line.charAt(i) === " ") break;
-        species += line.charAt(i);
-      }
-      const restOfLine = line.substring(i+1);
-      const observationsList = restOfLine.split(",");
-      for (const observation of observationsList) {
-        if (observation.includes("/")) {
-          const splitObs = observation.split("/");
-          switch (splitObs.length) {
-            case 0:
-            case 1:
-            case 2:
-          }
-        } else {
-          // asdf
-        }
-      }
-    }
-  }
-
 };
 
+export const getObservation = () => {
+  return taysiHavainto;
+};
+
+export const resetAll = () => {
+  firstSpaceOfLineBreakOrSomeSuchEncountered = false;
+  lisatietobucketIsOpen = false;
+  lajinimi = "";
+  lisatieto = "";
+  yksilomaara = "";
+  ika = "";
+  ohituspuoli = "";
+  ilmansuunta = "";
+  sukupuoliRound = 2;
+  ageBucket = "unk";
+  male = {
+    "subad": "",
+    "ad": "",
+    "juv": "",
+    "unk": ""
+  };
+  female = {
+    "subad": "",
+    "ad": "",
+    "juv": "",
+    "unk": ""
+  };
+  unknown = {
+    "subad": "",
+    "ad": "",
+    "juv": "",
+    "unk": ""
+  };
+  osahavainnot = [];
+  taysiHavainto = {};
+};
+
+export const specialTrimmer = (line) => {
+  return line.trim().toLowerCase();
+};
+
+export const checkBracketsFirstPass = (line) => {
+  const bracketsErr = new Error("sulut väärin");
+  let leftGuy = false;
+  for (let i = 0; i < line.length; i++) {
+    if (line[Number(i)] === "(") {
+      if (leftGuy) throw bracketsErr;
+      else leftGuy = true;
+    } else if (line[Number(i)] === ")") {
+      if (leftGuy) leftGuy = false;
+      else throw bracketsErr;
+    }
+  }
+  if (leftGuy) throw bracketsErr;
+};
+
+export const parse = (line) => {
+  const lineOfText = specialTrimmer(line);
+  checkBracketsFirstPass(lineOfText);
+  for (let i = 0; i < lineOfText.length; i++) {
+    giveMeABucket(lineOfText[Number(i)], lineOfText, i);
+  }
+  constructTaysiHavainto();
+  return getObservation();
+};
---
+++ frontend/src/shorthand/shorthand.test.js
@@ -0,0 +1,255 @@
+import {
+  parse,
+  resetAll
+} from "./shorthand";
+
+describe("Test algorithm with all the cases mentioned in the customer's docs", () => {
+
+  beforeEach(() => {
+    resetAll();
+  });
+
+  test("Extremely basic test", () => {
+    const lineOfText = "sommol 2/W";
+    console.log("line:", lineOfText);
+    const observation = parse(lineOfText);
+    expect(observation.species).toBe("sommol");
+    const { direction, unknownMaleCount, ...rest } = observation.osahavainnot[0];
+    expect(direction).toBe("w");
+    expect(unknownMaleCount).toBe("2");
+    for (const each of Object.values(rest)) {
+      expect(each).toBe("");
+    }
+  });
+
+  test("Harder test", () => {
+    const lineOfText = "Smol /4 E";
+    console.log("line:", lineOfText);
+    const observation = parse(lineOfText);
+
+    expect(observation.species).toBe("smol");
+    const { direction, unknownFemaleCount, ...rest } = observation.osahavainnot[0];
+    expect(direction).toBe("e");
+    expect(unknownFemaleCount).toBe("4");
+    for (const each of Object.values(rest)) {
+      expect(each).toBe("");
+    }
+  });
+
+  test("Third tier, still going", () => {
+    const lineOfText = "Smol /4 E (jp)";
+    console.log("line:", lineOfText);
+    const observation = parse(lineOfText);
+
+    expect(observation.species).toBe("smol");
+    const { direction, unknownFemaleCount, notes, ...rest } = observation.osahavainnot[0];
+    expect(direction).toBe("e");
+    expect(unknownFemaleCount).toBe("4");
+    expect(notes).toBe("jp");
+    for (const each of Object.values(rest)) {
+      expect(each).toBe("");
+    }
+  });
+
+  test("Fourth, god tier", () => {
+    const lineOfText = "Sommol 1\"2juv3subad/W";
+    console.log("line:", lineOfText);
+    const observation = parse(lineOfText);
+
+    expect(observation.species).toBe("sommol");
+    const { direction, juvenileMaleCount, adultMaleCount, subadultMaleCount, ...rest }
+       = observation.osahavainnot[0];
+    expect(direction).toBe("w");
+    expect(juvenileMaleCount).toBe("2");
+    expect(adultMaleCount).toBe("1");
+    expect(subadultMaleCount).toBe("3");
+    for (const each of Object.values(rest)) {
+      expect(each).toBe("");
+    }
+  });
+
+  test("Fifth, legacy mode", () => {
+    const lineOfText = "sm /1W, 2/E, 3/4w";
+    console.log("line:", lineOfText);
+    const observation = parse(lineOfText);
+
+    expect(observation.species).toBe("sm");
+    const { direction: direction0, unknownFemaleCount: unknownFemaleCount0, ...rest0 }
+       = observation.osahavainnot[0];
+    const { direction: direction1, unknownMaleCount: unknownMaleCount1, ...rest1 }
+       = observation.osahavainnot[1];
+    const { direction: direction2, unknownFemaleCount: unknownFemaleCount2,
+      unknownMaleCount: unknownMaleCount2, ...rest2 }
+       = observation.osahavainnot[2];
+
+    expect(unknownFemaleCount0).toBe("1");
+    expect(direction0).toBe("w");
+    expect(unknownMaleCount1).toBe("2");
+    expect(direction1).toBe("e");
+    expect(unknownMaleCount2).toBe("3");
+    expect(unknownFemaleCount2).toBe("4");
+    expect(direction2).toBe("w");
+    for (const each of Object.values(rest0)) {
+      expect(each).toBe("");
+    }
+    for (const each of Object.values(rest1)) {
+      expect(each).toBe("");
+    }
+    for (const each of Object.values(rest2)) {
+      expect(each).toBe("");
+    }
+  });
+
+  test("Sixth, no more tears", () => {
+    const lineOfText = " sm /1W, 2/E ,3/4w,";
+    console.log("line:", lineOfText);
+    const observation = parse(lineOfText);
+
+    expect(observation.species).toBe("sm");
+    expect(observation.osahavainnot[0].unknownFemaleCount).toBe("1");
+    expect(observation.osahavainnot[0].direction).toBe("w");
+    expect(observation.osahavainnot[1].unknownMaleCount).toBe("2");
+    expect(observation.osahavainnot[1].direction).toBe("e");
+    expect(observation.osahavainnot[2].unknownMaleCount).toBe("3");
+    expect(observation.osahavainnot[2].unknownFemaleCount).toBe("4");
+    expect(observation.osahavainnot[2].direction).toBe("w");
+  });
+
+  test("Seventh son", () => {
+    const lineOfText = "sm /1W 2E";
+    console.log("line:", lineOfText);
+    expect(() => {
+      parse(lineOfText);
+    }).toThrow("suunnan jälkeen tulee numero");
+  });
+
+  test("Eightball", () => {
+    const lineOfText = "sm /1/2W";
+    console.log("line:", lineOfText);
+    const observation = parse(lineOfText);
+
+    expect(observation.species).toBe("sm");
+    expect(observation.osahavainnot[0].unknownFemaleCount).toBe("1");
+    expect(observation.osahavainnot[0].direction).toBe("w");
+    expect(observation.osahavainnot[0].unknownUnknownCount).toBe("2");
+  });
+
+  test("Nine", () => {
+    const lineOfText = "sommol 1\"/2'sw";
+    console.log("line:", lineOfText);
+    const observation = parse(lineOfText);
+
+    expect(observation.species).toBe("sommol");
+    expect(observation.osahavainnot[0].adultMaleCount).toBe("1");
+    expect(observation.osahavainnot[0].juvenileFemaleCount).toBe("2");
+    expect(observation.osahavainnot[0].direction).toBe("sw");
+  });
+
+  test("Ten", () => {
+    const lineOfText = "grugru 100SW+-";
+    console.log("line:", lineOfText);
+    const observation = parse(lineOfText);
+
+    expect(observation.species).toBe("grugru");
+    expect(observation.osahavainnot[0].unknownUnknownCount).toBe("100");
+    expect(observation.osahavainnot[0].direction).toBe("sw");
+    expect(observation.osahavainnot[0].bypassSide).toBe("+-");
+  });
+
+  test("Eleven", () => {
+    const lineOfText = "grugru 100SW-+";
+    console.log("line:", lineOfText);
+    expect(() => {
+      parse(lineOfText);
+    }).toThrow("tuntematon ohituspuoli");
+  });
+
+  test("Twelve", () => {
+    const lineOfText = "grugru\n100-200SW+-";
+    console.log("line:", lineOfText);
+    expect(() => {
+      parse(lineOfText);
+    }).toThrow("ohituspuolen pitää olla viimeisenä");
+  });
+
+  test("Thirteen", () => {
+    const lineOfText = "grugru\n100SW+-,200 S +++\n, 300 \"W---";
+    console.log("line:", lineOfText);
+    const observation = parse(lineOfText);
+
+    expect(observation.species).toBe("grugru");
+    const { direction: direction0, unknownUnknownCount: unknownUnknownCount0,
+      bypassSide: bypassSide0, ...rest0 }
+    = observation.osahavainnot[0];
+    const { direction: direction1, unknownUnknownCount: unknownUnknownCount1,
+      bypassSide: bypassSide1, ...rest1 }
+    = observation.osahavainnot[1];
+    const { direction: direction2, adultUnknownCount: adultUnknownCount2,
+      bypassSide: bypassSide2, ...rest2 }
+    = observation.osahavainnot[2];
+
+    for (const each of Object.values(rest0)) {
+      expect(each).toBe("");
+    }
+    for (const each of Object.values(rest1)) {
+      expect(each).toBe("");
+    }
+    for (const each of Object.values(rest2)) {
+      expect(each).toBe("");
+    }
+
+    expect(unknownUnknownCount0).toBe("100");
+    expect(direction0).toBe("sw");
+    expect(bypassSide0).toBe("+-");
+    expect(unknownUnknownCount1).toBe("200");
+    expect(direction1).toBe("s");
+    expect(bypassSide1).toBe("+++");
+    expect(adultUnknownCount2).toBe("300");
+    expect(direction2).toBe("w");
+    expect(bypassSide2).toBe("---");
+  });
+
+  test("Fourteen", () => {
+    const lineOfText = "grugru 100SW ,,,\n,200S";
+    console.log("line:", lineOfText);
+    expect(() => {
+      parse(lineOfText);
+    }).toThrow("ylimääräisiä pilkkuja");
+  });
+
+  test("Fifteen", () => {
+    const lineOfText = "Smol /4 E (jp";
+    console.log("line:", lineOfText);
+    expect(() => {
+      parse(lineOfText);
+    }).toThrow("sulut väärin");
+  });
+
+  test("Sixteen", () => {
+    const lineOfText = "Smol /4 E jp)";
+    console.log("line:", lineOfText);
+    expect(() => {
+      parse(lineOfText);
+    }).toThrow("sulut väärin");
+  });
+
+  test("Seventeen", () => {
+    const lineOfText = "Smol /4 E ((jp))";
+    console.log("line:", lineOfText);
+    expect(() => {
+      parse(lineOfText);
+    }).toThrow("sulut väärin");
+  });
+
+  test("End of the world", () => {
+    const lineOfText = "Smol /4 E ((jp)";
+    console.log("line:", lineOfText);
+    expect(() => {
+      parse(lineOfText);
+    }).toThrow("sulut väärin");
+  });
+
+
+
+});
+
