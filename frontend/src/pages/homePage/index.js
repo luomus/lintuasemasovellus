abc57172e946b88546a68db3e9e4f1a10c1a17e7
Arttu Mykkänen <arttu.mykkanen@outlook.com>
2020-11-05 20:58:45 +0200

    Fix crash bug when date-field is emptied

--- frontend/src/pages/homePage/index.js
+++ frontend/src/pages/homePage/index.js
@@ -73,6 +73,7 @@ export const HomePage = () => {
   };
 
   const formatDate = (date) => {
+    if (!date) return "";
     const dd = date.getDate();
     const mm = date.getMonth() + 1;
     return `${dd > 9 ? "" : "0"}${dd}.${mm > 9 ? "" : "0"}${mm}.${date.getFullYear()}`;
