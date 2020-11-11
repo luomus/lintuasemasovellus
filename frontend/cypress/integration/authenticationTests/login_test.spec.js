444ea6399f1fa4127b322271c60c123b72a2856f
Annukka Närhi <annukka.narhi@gmail.com>
2020-11-11 16:13:19 +0200

    Cypress test fix started, need more fixing

--- frontend/cypress/integration/authenticationTests/login_test.spec.js
+++ frontend/cypress/integration/authenticationTests/login_test.spec.js
@@ -1,6 +1,10 @@
 describe("Login", function() {
   beforeEach(function() {
     cy.visit("http://localhost:3000");
+    cy.request("http://localhost:5000/testlogin?token=MzJkNTVkMjAtZTFjZS00NzEzLTlkM2MtMmRjZGI1ODYyNGUw");
+
+    cy.visit("http://localhost:3000");
+
   });
   it("Front page redirects to login page if user is not logged in", function() {
 
@@ -28,7 +32,9 @@ describe("Login", function() {
         },
       });
 
-    cy.get("#select-observatory").click().get("ul > li").eq(0).click();
+    //cy.get("#select-observatory").click().get("ul > li").eq(0).click();
+    cy.get("#select-observatory").click();
+    cy.contains("Hangon Lintuasema").click();
     cy.get("#submit").contains("Tallenna").click();
     cy.get("#logout-link").click();
     //cy.get("#login-link");
