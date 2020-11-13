ae123e396a625092855800e2a31453d5e79cd9a5
Jyrki <jyrki.kaukoranta@helsinki.fi>
2020-11-13 15:47:21 +0200

    soft delete comment and observer

--- lintuasema-backend/app/api/classes/observationperiod/services.py
+++ lintuasema-backend/app/api/classes/observationperiod/services.py
@@ -5,3 +5,14 @@ def getObsPerId(starttime, endtime, location_id, day_id):
     obsp = Observationperiod.query.filter_by(startTime = starttime, endTime=endtime, location_id=location_id, day_id=day_id).first()
     return obsp.id
 
+def setObsPerDayId(day_id_old, day_id_new):
+    obsp = Observationperiod.query.filter_by(day_id = day_id_old).all()
+    for x in obsp:
+        obs = Observationperiod(startTime = x.startTime, endTime = x.endTime, typeId = x.typeId, locationId = x.locationId, day_Id = x.dayId)
+        addObservationperiod(obs)
+        x.is_deleted = 1
+
+def addObservationperiod(observationperiod):
+        db.session().add(observationperiod)
+        db.session().commit()
+
