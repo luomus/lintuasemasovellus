aa4bd4d97b858b934f14a5df45f6844540c41122
Pekka <pekka.laine@gmail.com>
2020-11-25 14:33:44 +0200

    Robert, Annu, Pekka: birds.json modified

---
+++ frontend/src/shorthand/skripti.py
@@ -0,0 +1,7 @@
+with open('birdsNew.json', 'r') as f, open('output.txt', 'w') as fo:
+    for line in f:
+        if "key" in line:
+            x = line.split('"')
+            fo.write("    "+ '"'+x[3]+'"' + " : {\n")
+        elif "{" not in line:
+            fo.write(line)
