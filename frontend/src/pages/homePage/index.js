7ea2bdad7a0a129c69d58b5e2da8edd038fecf5f
Joel Wang <workworkjoel@gmail.com>
2020-11-25 14:18:43 +0200

    spacing fix

--- frontend/src/pages/homePage/index.js
+++ frontend/src/pages/homePage/index.js
@@ -45,7 +45,7 @@ const useStyles = makeStyles((theme) => ({
   },
   formControl: {
     margin: theme.spacing(0),
-    minWidth: 140,
+    minWidth: 120,
   },
   sendButton: {
     marginBottom: "40px"
