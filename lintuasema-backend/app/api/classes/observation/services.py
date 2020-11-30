b6ae22687816d7a1f08172631abce3e146608414
Annukka Närhi <annukka.narhi@gmail.com>
2020-11-30 14:44:23 +0200

    Observation views refactoring finished

--- lintuasema-backend/app/api/classes/observation/services.py
+++ lintuasema-backend/app/api/classes/observation/services.py
@@ -1,6 +1,7 @@
 from app.api.classes.observation.models import Observation
 from app.db import db
 from flask import jsonify
+from sqlalchemy.sql import text
 
 def parseCountString(obs):
     
@@ -63,3 +64,40 @@ def addObservationToDb(req):
 def deleteObservation(shorthand_id):
     observation = Observation.query.filter_by(shorthand_id).first()
     observation.is_deleted = 1
+
+
+def getDaySummary(day_id):
+    stmt = text("SELECT Observation.species,"
+                " SUM(CASE WHEN (Type.name = :const OR Type.name = :other OR Type.name = :night OR Type.name = :scatter) THEN total_count ELSE 0 END) AS allMigration,"
+                " SUM(CASE WHEN Type.name = :const THEN total_count ELSE 0 END) AS constMigration,"
+                " SUM(CASE WHEN Type.name = :other THEN total_count ELSE 0 END) AS otherMigration,"
+                " SUM(CASE WHEN Type.name = :night THEN total_count ELSE 0 END) AS nightMigration,"
+                " SUM(CASE WHEN Type.name = :scatter THEN total_count ELSE 0 END) AS scatterObs,"
+                " SUM(CASE WHEN Type.name = :local THEN total_count ELSE 0 END) AS totalLocal,"
+                " SUM(CASE WHEN (Type.name = :local AND Location.name <> :gou) THEN total_count ELSE 0 END) AS LocalOther,"
+                " SUM(CASE WHEN (Type.name = :local AND Location.name = :gou) THEN total_count ELSE 0 END) AS LocalGou"
+                " FROM Observation"
+                " LEFT JOIN Observationperiod ON Observationperiod.id = Observation.observationperiod_id"
+                " LEFT JOIN Type ON Type.id = Observationperiod.type_id"
+                " LEFT JOIN Location ON Location.id = Observationperiod.location_id"
+                " WHERE Observationperiod.day_id = :day_id"
+                " GROUP BY Observation.species").params(day_id = day_id, 
+                    const = "Vakio", other = "Muu muutto", night = "Yömuutto", scatter = "Hajahavainto",
+                    local = "Paikallinen", gou = "Luoto Gåu")
+
+    res = db.engine.execute(stmt)
+
+    response = []
+
+    for row in res:
+        response.append({"species" :row[0], 
+            "allMigration":row[1],
+            "constMigration":row[2], 
+            "otherMigration":row[3],
+            "nightMigration":row[4],
+            "scatterObs":row[5],
+            "totalLocal":row[6],
+            "localOther":row[7],
+            "localGåu":row[8]})
+  
+    return jsonify(response) 
--- lintuasema-backend/app/api/classes/observation/views.py
+++ lintuasema-backend/app/api/classes/observation/views.py
@@ -4,7 +4,7 @@ from flask_login import login_required
 
 from app.api.classes.observation.models import Observation
 from app.api.classes.observationperiod.models import Observationperiod
-from app.api.classes.observation.services import parseCountString, addObservationToDb
+from app.api.classes.observation.services import parseCountString, addObservationToDb, getDaySummary
 
 from app.api import bp
 from app.db import db
@@ -39,16 +39,7 @@ def getObservations():
 @bp.route('/api/getObservations/<observationperiod_id>', methods=["GET"]) 
 @login_required
 def getObservationsByObservationPeriod(observationperiod_id):
-    # observations = Observation.query.filter_by(observationperiod_id = observationperiod_id)
-    # ret = []
-    # for each in observations:
-    #     ret.append({ 'species': each.species, 'adultUnknownCount': each.adultUnknownCount, 'adultFemaleCount': each.adultFemaleCount, 'adultMaleCount': each.adultMaleCount,
-    #         'juvenileUnknownCount': each.juvenileUnknownCount, 'juvenileFemaleCount': each.juvenileFemaleCount, 'juvenileMaleCount': each.juvenileMaleCount,
-    #         'subadultUnknownCount': each.subadultUnknownCount, 'subadultFemaleCount': each.subadultFemaleCount, 'subadultMaleCount': each.subadultMaleCount,
-    #         'unknownUnknownCount': each.unknownUnknownCount, 'unknownFemaleCount': each.unknownFemaleCount, 'unknownMaleCount': each.unknownMaleCount, 'direction': each.direction, 'bypassSide': each.bypassSide, 'notes': each.notes, 'observationperiod_id': each.observationperiod_id})
-
-    # return jsonify(ret)
-
+    
     observations = Observation.query.filter_by(observationperiod_id = observationperiod_id)
     ret = []
     for observation in observations:
@@ -61,10 +52,8 @@ def getObservationsByObservationPeriod(observationperiod_id):
 @login_required
 def observations_delete():
     req = request.get_json()
-    print(req)
     shorthand_id = req['shorthand_id']
     Observation.query.filter_by(shorthand_id=shorthand_id).delete()
-    #db.session.query(Observation).filter(Observation.shorthand_id == shorthand_id).delete()
     db.session.commit()
     return jsonify(req)
 
@@ -72,38 +61,8 @@ def observations_delete():
 @bp.route('/api/getObservationSummary/<day_id>', methods=["GET"])
 @login_required
 def getSummary(day_id):
-    stmt = text("SELECT Observation.species,"
-                " SUM(CASE WHEN (Type.name = :const OR Type.name = :other OR Type.name = :night OR Type.name = :scatter) THEN total_count ELSE 0 END) AS allMigration,"
-                " SUM(CASE WHEN Type.name = :const THEN total_count ELSE 0 END) AS constMigration,"
-                " SUM(CASE WHEN Type.name = :other THEN total_count ELSE 0 END) AS otherMigration,"
-                " SUM(CASE WHEN Type.name = :night THEN total_count ELSE 0 END) AS nightMigration,"
-                " SUM(CASE WHEN Type.name = :scatter THEN total_count ELSE 0 END) AS scatterObs,"
-                " SUM(CASE WHEN Type.name = :local THEN total_count ELSE 0 END) AS totalLocal,"
-                " SUM(CASE WHEN (Type.name = :local AND Location.name <> :gou) THEN total_count ELSE 0 END) AS LocalOther,"
-                " SUM(CASE WHEN (Type.name = :local AND Location.name = :gou) THEN total_count ELSE 0 END) AS LocalGou"
-                " FROM Observation"
-                " LEFT JOIN Observationperiod ON Observationperiod.id = Observation.observationperiod_id"
-                " LEFT JOIN Type ON Type.id = Observationperiod.type_id"
-                " LEFT JOIN Location ON Location.id = Observationperiod.location_id"
-                " WHERE Observationperiod.day_id = :day_id"
-                " GROUP BY Observation.species").params(day_id = day_id, 
-                    const = "Vakio", other = "Muu muutto", night = "Yömuutto", scatter = "Hajahavainto",
-                    local = "Paikallinen", gou = "Luoto Gåu")
-
-    res = db.engine.execute(stmt)
-
-    response = []
-
-    for row in res:
-        response.append({"species" :row[0], 
-            "allMigration":row[1],
-            "constMigration":row[2], 
-            "otherMigration":row[3],
-            "nightMigration":row[4],
-            "scatterObs":row[5],
-            "totalLocal":row[6],
-            "localOther":row[7],
-            "localGåu":row[8]})
+    
+    response = getDaySummary(day_id)
   
-    return jsonify(response)
+    return response
   
