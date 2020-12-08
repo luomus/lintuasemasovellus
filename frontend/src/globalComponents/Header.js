24b7260094852def5ac0758ce37543dfcd1fb768
Annukka Närhi <annukka.narhi@gmail.com>
2020-12-08 22:09:33 +0200

    A ref changed to Link so hawk-logo goes to right place

--- frontend/src/globalComponents/Header.js
+++ frontend/src/globalComponents/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 //import { useTranslation } from "react-i18next";
 import haukkalogo from "../resources/haukkalogo_white.png";
 
@@ -7,14 +8,14 @@ const Header = () => {
   //const { t } = useTranslation();
 
   return (
-    <a href="/">
+    <Link to="/">
       <img
         src={haukkalogo}
         height="35"
         alt="haukka"
       >
       </img>
-    </a>
+    </Link>
   );
 };
 
