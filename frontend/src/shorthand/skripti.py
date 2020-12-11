ae6376a0cf63cfdd0d8f84492286e9dcca1e2e0c
Annukka Närhi <annukka.narhi@gmail.com>
2020-12-11 16:18:46 +0200

    birds.json should now contain all the birds

--- frontend/src/shorthand/skripti.py
+++ frontend/src/shorthand/skripti.py
@@ -1,4 +1,4 @@
-with open('birdsNew.json', 'r') as f, open('output.txt', 'w') as fo:
+with open('birdlonger.json', 'r') as f, open('output.txt', 'w') as fo:
     for line in f:
         if "key" in line:
             x = line.split('"')
