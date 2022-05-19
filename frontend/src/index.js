e42a4001d88130d2e9e56804446a3a8d2001b1af
Teemu Lintula <tl0@users.noreply.github.com>
2022-05-19 12:07:22 +0300

    change to HashRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./i18n";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme/themeSettings";
