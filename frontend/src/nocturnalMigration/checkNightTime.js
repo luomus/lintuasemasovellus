103b7c7dba7a008ff48cd680a452b9dba4bed2d5
Joel6677 <joel.heusala@gmail.com>
2022-06-03 16:53:33 +0300

    calculate sunset and sunrise plus testit

--- frontend/src/nocturnalMigration/checkNightTime.js
+++ frontend/src/nocturnalMigration/checkNightTime.js
@@ -1,10 +1,16 @@
-import { getHankoSunset, getHankoSunrise, getJurmoSunrise, getJurmoSunset } from "../../src/services";
+import { getHankoSunset, getHankoSunrise, getJurmoSunrise, getJurmoSunset } from "../services";
 
 const isNightHanko = async (currentTime) => {
   const hankoSunset = new Date(await getHankoSunset());
   const hankoSunrise = new Date(await getHankoSunrise());
 
+  console.log("current time: ", currentTime);
+  console.log("hanko sunset: ", hankoSunset);
+  console.log("hanko sunrise: ", hankoSunrise);
+  console.log("is Night: ", currentTime >= hankoSunset && currentTime < hankoSunrise);
+
   if (currentTime >= hankoSunset && currentTime < hankoSunrise) {
+    console.log("true");
     return true;
   }
 };
@@ -21,9 +27,9 @@ const isNightJurmo = async (currentTime) => {
 export const isNightTime = (observatory ) => {
 
   const currentTime = new Date();
-  // let currentTime = new Date("June 2, 2022 03:24:00");
+  // let currentTime = new Date("June 3, 2022 03:24:00");
   // console.log("current time: ", currentTime);
-  // console.log("observatory: ", observatory);
+  console.log("observatory: ", observatory);
 
   if (observatory === "Hangon_Lintuasema" && isNightHanko(currentTime)) {
     return true;
--- frontend/src/test/nocturnalMigration.test.js
+++ frontend/src/test/nocturnalMigration.test.js
@@ -1,10 +1,41 @@
-import { checkNightTime } from "../nocturnalMigration/checkNightTime.js";
+import { isNightTime } from "../nocturnalMigration/checkNightTime.js";
+import { getSunrise, getSunset } from "../nocturnalMigration/calculateSunsetAndSunrise.js";
 
-describe.skip("night time observation tests", () => {
+describe.skip("is night time tests", () => {
 
-  test("test checkNightTime method", () => {
-    console.log(checkNightTime());
-    expect(checkNightTime()).toBe(false);
+  test("test checkNightTime method if day", () => {
+    console.log("is night time: ", isNightTime("Hangon_Lintuasema"));
+    expect(isNightTime("Hangon_Lintuasema")).toBe(false);
+  });
+
+  test.skip("test checkNightTime method if night", () => {
+    expect(isNightTime("Hangon_Lintuasema")).toBe(true);
+  });
+
+  test.skip("test checkNightTime method if day", () => {
+    expect(isNightTime("Jurmon_Lintuasema")).toBe(false);
+  });
+
+  test.skip("test checkNightTime method if night", () => {
+    expect(isNightTime("Jurmon_Lintuasema")).toBe(true);
+  });
+
+});
+
+describe("calculate sunset/sunrise tests", () => {
+
+  test("test getting sunrise from hanko", () => {
+    const date = new Date();
+    const hankoLatitude = "59.832394";
+    const hankoLongitude = "22.970695";
+    console.log("Get sunrise hanko: ", getSunrise(hankoLatitude, hankoLongitude, date));
+  });
+
+  test("test getting sunset from hanko", () => {
+    const date = new Date();
+    const hankoLatitude = "59.832394";
+    const hankoLongitude = "22.970695";
+    console.log("Get sunset hanko: ", getSunset(hankoLatitude, hankoLongitude, date));
   });
 
 });
