f494c9dc3d0803f8f7a93518860a69ae1e59a26d
Teemu Lintula <6422236+tl0@users.noreply.github.com>
2022-06-21 12:46:45 +0300

    add healthz backend route
    Could be used possibly for health checks by eg. k8s.
    Currently only checks for if database can be reached.

--- lintuasema-backend/application/api/routes.py
+++ lintuasema-backend/application/api/routes.py
@@ -1,8 +1,18 @@
 from flask import make_response
 from application.api import bp
+from application.db import db
 
 @bp.route('/api', methods=['GET'])
 def hello_world():
     response = make_response("Hello world", 200)
     response.mimetype = "text/plain"
     return response
+
+@bp.route('/api/healthz', methods=['GET'])
+def healthz():
+    try:
+        db.session.execute("SELECT 1")
+    except Exception as e:
+        print(e)
+        return make_response("Database is not OK", 500)
+    return make_response("OK", 200)
