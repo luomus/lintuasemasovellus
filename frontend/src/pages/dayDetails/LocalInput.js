9bd3c7b9c2af612179925e3a14b969c59b5ba760
Joel6677 <joel.heusala@gmail.com>
2022-06-27 15:09:33 +0300

    deleting commented code

--- frontend/src/pages/dayDetails/LocalInput.js
+++ frontend/src/pages/dayDetails/LocalInput.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { TextField, makeStyles } from "@material-ui/core";
 import { updateLocalObservation } from "../../services";
-// import { editTotalCount, editLocalCount, editLocalGåuCount, getTotalCount, getLocalCount, getLocalGåuCount } from "../../services";
 import PropTypes from "prop-types";
 const useStyles = makeStyles({
   textInput: {
@@ -11,19 +10,11 @@ const useStyles = makeStyles({
 
 const LocalInput = (props) => {
 
-  useEffect(() => {
-    // setLocalCount(getLocalCount)
-    // setLocalGåuCount(getLocalGåuCount)
-    // setTotalCount(getLocalTotalCount)
-  },[]);
   const [date] = useState(props.date);
   const [observatory] = useState(props.observatory);
   const [value, setValue] = useState(props.count);
   const [species] = useState(props.species);
   const [gau] = useState(props.gau);
-  // const [totalCount, setTotalCount] = useState(0);
-  // const [localCount, setLocalCount] = useState(0);
-  // const [localGåuCount, setLocalGåuCount] = useState(0);
 
   const classes = useStyles();
 
@@ -31,9 +22,6 @@ const LocalInput = (props) => {
     setValue(event.target.value);
     props.onChange();
     updateLocalObservation(date, observatory, species, event.target.value, gau);
-    // editTotalCount(totalCount).then(totalCountJson => setTotalCount(totalCountJson.data.totalCount));)
-    // editLocalCount(localCount).then(localCountJson => setLocalCount(localCountJson.data.localCount));
-    // editLocalGåuCount(localGåuCount).then(localGåuCountJson => setLocalGåuCount(localGåuCountJson.data.localGåuCount));
   };
 
   return(
