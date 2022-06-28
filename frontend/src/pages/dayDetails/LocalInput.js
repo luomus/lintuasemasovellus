293de29e3aa30a7f3c8f94c176859ce40815cf71
Joel6677 <joel.heusala@gmail.com>
2022-06-28 11:54:45 +0300

    loading pallero works

--- frontend/src/pages/dayDetails/LocalInput.js
+++ frontend/src/pages/dayDetails/LocalInput.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
-import { TextField, makeStyles } from "@material-ui/core";
+import { TextField, makeStyles, CircularProgress } from "@material-ui/core";
 import { updateLocalObservation } from "../../services";
 import PropTypes from "prop-types";
 const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
   textInput: {
     width: "50px",
+  },
+  loadingCircle: {
+    marginRight: "10px"
   }
 });
 
@@ -15,22 +23,33 @@ const LocalInput = (props) => {
   const [value, setValue] = useState(props.count);
   const [species] = useState(props.species);
   const [gau] = useState(props.gau);
+  const [showCircularProgress, setShowCircularProgress] = useState(false);
 
   const classes = useStyles();
 
-  const handleInput = (event) => {
+  const handleInput = async (event) => {
+    setShowCircularProgress(true);
     setValue(event.target.value);
-    props.onChange();
-    updateLocalObservation(date, observatory, species, event.target.value, gau);
+    // props.onChange();
+    try {
+      await updateLocalObservation(date, observatory, species, event.target.value, gau);
+    } catch (error) {
+      console.log("error: ", error);
+      alert("Tallennus epäonnistui!");
+    }
+    setShowCircularProgress(false);
   };
 
   return(
-    <TextField id="standard-basic" className={classes.textInput}
-      variant="standard" type="number" size="small" value={value} species={species} onChange={(event) => handleInput(event)} InputProps={{
-        inputProps: {
-          min: 0
-        }
-      }}/>
+    <div className={classes.container}>
+      {showCircularProgress && <CircularProgress className={classes.loadingCircle} size={30} />}
+      <TextField id="standard-basic" className={classes.textInput}
+        variant="standard" type="number" size="small" value={value} species={species} onChange={(event) => handleInput(event)} InputProps={{
+          inputProps: {
+            min: 0
+          }
+        }}/>
+    </div>
   );
 };
 
