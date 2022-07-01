b41dfd100bf352707b40b4a3bf28f1cf16da7bef
Toni Rämö <toni.ramo@helsinki.fi>
2022-07-01 15:07:42 +0300

    Clear unnecessary comments

--- frontend/src/pages/dayDetails/LocalInput.js
+++ frontend/src/pages/dayDetails/LocalInput.js
@@ -1,5 +1,5 @@
-import React /*, { useState }*/ from "react";
-import { TextField, makeStyles/*, CircularProgress*/ } from "@material-ui/core";
+import React from "react";
+import { TextField, makeStyles } from "@material-ui/core";
 import { updateLocalObservation, updateScatterObservation } from "../../services";
 import PropTypes from "prop-types";
 
@@ -19,17 +19,12 @@ const useStyles = makeStyles({
 
 const LocalInput = ({ date, observatory, count, species, dataType, onChange, inputRef }) => {
 
-  //const [showCircularProgress, setShowCircularProgress] = useState(false);
-
   const classes = useStyles();
 
   const handleInput = async (event) => {
     event.preventDefault();
     if (parseInt(event.target.value) !== count && Number.isInteger(parseInt(event.target.value))) {
       onChange(parseInt(event.target.value));
-      //setTimeout(() => {
-      // setShowCircularProgress(true);
-      //}, 1500);
       try {
         if (dataType.includes("local")) {
           await updateLocalObservation(date, observatory, species, event.target.value, dataType === "localGau" ? 1 : 0);
@@ -41,15 +36,11 @@ const LocalInput = ({ date, observatory, count, species, dataType, onChange, inp
         console.log("error: ", error);
         alert("Tallennus epäonnistui!");
       }
-      //setTimeout(() => {
-      //  setShowCircularProgress(false);
-      //}, 2000);
     }
   };
 
   return (
     <div className={classes.container}>
-      {/*showCircularProgress && <CircularProgress className={classes.loadingCircle} size={30} />*/}
       <TextField id="standard-basic" name={dataType} className={classes.textInput} ref={inputRef}
         variant="standard" type="number" size="small" species={species} onBlur={handleInput} InputProps={{
           inputProps: {
