339f206b0f026155f7104f3be404e2b527198f03
aorin <meeri.rannisto@gmail.com>
2024-11-22 18:02:24 +0200

    fix cypress login issues

--- frontend/cypress/e2e/methods.js
+++ frontend/cypress/e2e/methods.js
@@ -1,40 +1,12 @@
 
-
 export function myBeforeEach() {
-  cy.wait(1000);
-  cy.visit("http://localhost:3000");
-
   const personToken = Cypress.env("person_token");
   const loginUrl = `http://localhost:3000/api/login?token=${personToken}`;
 
-  if (loginPort === 3000) {
-    cy.visit(loginUrl);
-  } else {
-    cy.request(loginUrl);
-  }
+  cy.visit(loginUrl);
 
-  cy.visit("http://localhost:3000");
-
-
-
-
-  const user = {
-    id: "asdf",
-    fullName: "Lintu Asema",
-    emailAddress: "lintuasema@lintuasema.com"
-  };
-
-
-  cy.window()
-    .its("store")
-    .invoke("dispatch", {
-      type: "SET_USER",
-      data: {
-        user
-      },
-    });
-
-  cy.get("body").wait(1000)
+  cy.get("#select-observatory,#observatorySelector",{ timeout: 5000 }).should("exist");
+  cy.get("body")
     .then(($body) => {
       if ($body.text().includes("Valitse")) {
         cy.get("#select-observatory").click();
