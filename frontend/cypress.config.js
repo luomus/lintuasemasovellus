b6229908d24116e91029b038a4d0e356bb391b8f
aorin <meeri.rannisto@gmail.com>
2024-11-25 10:37:11 +0200

    enable cypress video recording

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,8 +1,23 @@
 const { defineConfig } = require('cypress')
+const fs = require('fs')
 
 module.exports = defineConfig({
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
+      on('after:spec', (spec, results) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some((test) =>
+            test.attempts.some((attempt) => attempt.state === 'failed')
+          )
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video)
+          }
+        }
+      })
+
       const legacyConfig = require('./cypress/plugins/index.js')(on, config);
 
       if (config.isTextTerminal) {
@@ -12,7 +27,7 @@ module.exports = defineConfig({
           excludeSpecPattern: ['cypress/e2e/all.cy.js'],
         }
       }
-      
+
       return legacyConfig;
     }
   },
